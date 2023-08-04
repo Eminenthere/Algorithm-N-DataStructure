cbb9259078597ca53032001e4e4a88dffc9fe2ea
eminentishere <eminentishere@outlook.com>
2023-08-04 23:43:22 +0500

    Fibonnaci Algorithm

---
+++ Fibonacci.js
@@ -0,0 +1,14 @@
+const fibonacci = (n) => {
+  const fib = [0, 1]
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+  }
+  return fib
+}
+
+// BigO - O(n)
+
+console.log(fibonacci(2))
+console.log(fibonacci(3))
+console.log(fibonacci(7))
